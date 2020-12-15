2e96f170b02c0610604acb492cf7b1d30be9109c
Lindsay Deaton <lindsay-deaton@lambdastudents.com>
2020-12-14 22:23:26 -0500

    finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
+//imported data similar to function fetchStock
 
-import SavedList from './Movies/SavedList';
+import { Link, Route, Switch } from "react-router-dom";
+//alows you to do a link route and a switch
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+        .then((response) => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(res.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        {/* <Link to "/" </Link>
+        <Link to "/movies/">Movies</Link> */}
+        Replace this Div with your Routes
+      </div>
+
+      <Switch>
+        <Route path="/movie/:id">
+          <Movie />
+        </Route>
+
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
+import Movie from "./Movie";
+import { useHistory } from "react-router-dom";
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {props.movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -11,10 +13,11 @@ export default function MovieList(props) {
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory();
 
   return (
-    <div className="movie-card">
+    <div onClicks={() => history.push(`movie/${id}`)} className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
